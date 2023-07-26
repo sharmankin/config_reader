327ac39c99b37ba3392b6049959bdac0d991e3d7
karpo <me@karpovan.ru>
2023-07-26 23:43:15 +0300

    v0.0.7

--- config_reader/read.py
+++ config_reader/read.py
@@ -1,10 +1,9 @@
 import os
 import platform
-import re
-from pathlib import Path
 import stat
+from pathlib import Path
 
-from dotenv import load_dotenv, find_dotenv, set_key
+from dotenv import load_dotenv, find_dotenv
 
 env = Path(find_dotenv('project.env'))
 
@@ -13,28 +12,6 @@ assert load_dotenv(env), 'No project.env found'
 project_root = Path(
     env.parent
 )
-
-project_in_path = False
-
-if python_path := os.getenv('PYTHONPATH'):
-
-    path_options = [
-        project_root.as_posix()
-    ]
-
-    if platform.system() == 'Windows':
-        dl, *parts = project_root.parts
-        path_options += [dl.replace('\\', r'\\') + r'\\'.join(parts)]
-
-    path_pattern = '(?:' + '|'.join(path_options) + ')'
-
-    project_in_path = bool(
-        re.search(r'(?<=[:;])|(?<=^)' + path_pattern + r'(?=;|:|$)', python_path)
-    )
-
-if not project_in_path:
-    set_key(env, 'PYTHONPATH', project_root.as_posix(), quote_mode='auto', export=True)
-    load_dotenv(env)
 
 
 def get_config(node_path: str, *, conf_string_delimiter: str = None, **kwargs):
